0e4a37153736464857c2c56a19188ae8188b163c
remon <remonmizanur@gmail.com>
2023-01-29 13:07:34 +0600

    image zoom

--- app/src/main/java/com/example/dummysocial/View/PostDetailsActivity.kt
+++ app/src/main/java/com/example/dummysocial/View/PostDetailsActivity.kt
@@ -10,6 +10,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -19,11 +20,14 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
@@ -32,6 +36,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.DialogProperties
 import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.compose.rememberNavController
@@ -66,6 +71,7 @@ class PostDetailsActivity : ComponentActivity() {
     var height: Int = 0
     var width: Int = 0
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -85,15 +91,15 @@ class PostDetailsActivity : ComponentActivity() {
         //checkNetwork()
         //(application as DaggerApplication).applicationComponent.inject(this)
         setContent {
+
+
             DummySocialTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
                 ) {
 
                     MainUI()
-
 
                 }
             }
@@ -109,31 +115,53 @@ class PostDetailsActivity : ComponentActivity() {
 
         Scaffold {
 
-            when (val result = postDetailsViewModel.response.value) {
-                is ApiState.SuccessPostDetails -> {
-                    Log.d("dataxx", "getPostDetsils ACT: ${result.data.text}")
+            Column() {
+                loadPostDetails()
 
-                    getData(result.data)
-                }
+                loadComment()
 
-                is ApiState.Failure -> {
-                    Log.d("dataxx", "getData: ${result.msg.toString()}")
-                }
 
-                ApiState.Loading -> {
-                    MyCircularProgress()
-                }
+            }
+        }
+    }
 
-                ApiState.Empty -> {
+    @Composable
+    private fun loadPostDetails() {
+        when (val result = postDetailsViewModel.response.value) {
+            is ApiState.SuccessPostDetails -> {
+                Log.d("dataxx", "getPostDetsils ACT: ${result.data.text}")
 
-                }
+                getData(result.data)
             }
 
+            is ApiState.Failure -> {
+                Log.d("dataxx", "getData: ${result.msg.toString()}")
+            }
+
+            ApiState.Loading -> {
+                MyCircularProgress()
+            }
+
+            ApiState.Empty -> {
+
+            }
         }
     }
 
     @Composable
     private fun getData(response: PostDetails_response) {
+        var dialogState by remember {
+            mutableStateOf(false)
+        }
+
+        var imageUrl by remember {
+            mutableStateOf("")
+        }
+
+        largeImageDialog(dialogState = dialogState, imageUrl = imageUrl, onDismissRequest = {
+            dialogState = !it
+        })
+
         Column(verticalArrangement = Arrangement.spacedBy(5.dp)) {
             Row(
                 modifier = Modifier.padding(start = 5.dp, end = 5.dp, top = 10.dp, bottom = 5.dp),
@@ -163,13 +191,9 @@ class PostDetailsActivity : ComponentActivity() {
                     Text(
                         text = " ${
                             changeDateFormat(
-                                "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'",
-                                "MMM d, yyyy",
-                                response.publishDate
+                                "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", "MMM d, yyyy", response.publishDate
                             )
-                        }",
-                        fontSize = 10.sp,
-                        color = Color.Gray
+                        }", fontSize = 10.sp, color = Color.Gray
                     )
                 }
             }
@@ -180,14 +204,27 @@ class PostDetailsActivity : ComponentActivity() {
                 modifier = Modifier.padding(start = 10.dp, end = 10.dp)
             )
 
-            Image(
-                painter = rememberImagePainter(response.image),
-                contentDescription = null,
-                contentScale = ContentScale.Crop,
-                modifier = Modifier
-                    .size(ScreenSize.width().dp, 200.dp),
-
+            Box(modifier = Modifier.size(ScreenSize.width().dp, 200.dp)) {
+                Image(
+                    painter = rememberImagePainter(response.image),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
                 )
+
+                IconButton(
+                    onClick = {
+                        dialogState = true
+                        imageUrl = response.image.toString()
+                    },
+                    modifier = Modifier.align(Alignment.BottomEnd),
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_baseline_fullscreen_24),
+                        tint = colorResource(id = R.color.white),
+                        contentDescription = "fullscreen"
+                    )
+                }
+            }
 
             Row(verticalAlignment = Alignment.CenterVertically) {
                 IconButton(onClick = { /*TODO*/ }) {
@@ -224,7 +261,6 @@ class PostDetailsActivity : ComponentActivity() {
                 modifier = Modifier.padding(10.dp)
             )
 
-            loadComment()
 
         }
     }
@@ -262,7 +298,8 @@ class PostDetailsActivity : ComponentActivity() {
     private fun getCommentAdapter(response: Data) {
 
 
-        val cardColor = if (isNightMode(context = LocalContext.current)) R.color.status_color else R.color.Azure
+        val cardColor =
+            if (isNightMode(context = LocalContext.current)) R.color.status_color else R.color.Azure
 
         Row(
             modifier = Modifier.padding(top = 5.dp, bottom = 5.dp, start = 20.dp, end = 20.dp),
@@ -284,8 +321,7 @@ class PostDetailsActivity : ComponentActivity() {
 
             Card(
                 //modifier = Modifier.fillMaxWidth(),
-                elevation = 0.dp,
-                backgroundColor = colorResource(
+                elevation = 0.dp, backgroundColor = colorResource(
                     id = cardColor
                 )
             ) {
@@ -310,6 +346,56 @@ class PostDetailsActivity : ComponentActivity() {
 
     }
 
+    @Composable
+    private fun largeImageDialog(
+        dialogState: Boolean, imageUrl: String, onDismissRequest: (dialogState: Boolean) -> Unit
+    ) {
+        val scale = remember { mutableStateOf(1f) }
+        val rotationState = remember { mutableStateOf(1f) }
+
+        if (dialogState) {
+            AlertDialog(title = null,
+                text = null,
+                onDismissRequest = { onDismissRequest(dialogState) },
+                properties = DialogProperties(
+                    dismissOnBackPress = true, dismissOnClickOutside = true
+                ),
+                buttons = {
+                    Box(modifier = Modifier.pointerInput(Unit) {
+                        detectTransformGestures { centroid, pan, zoom, rotation ->
+                            scale.value *= zoom
+                            rotationState.value += rotation
+                        }
+                    }) {
+                        Image(
+                            rememberImagePainter(imageUrl),
+                            contentDescription = "imageZoom",
+                            modifier = Modifier
+                                .width(ScreenSize.width().dp)
+                                .height((ScreenSize.height() / 2).dp)
+                                .graphicsLayer(
+                                    // adding some zoom limits (min 50%, max 200%)
+                                    scaleX = maxOf(.5f, minOf(3f, scale.value)),
+                                    scaleY = maxOf(.5f, minOf(3f, scale.value)),
+                                    //rotationZ = rotationState.value
+                                ),
+                            contentScale = ContentScale.Crop,
+
+                            //    modifier = Modifier.fillMaxSize()
+                        )
+                        TextButton(
+                            onClick = { onDismissRequest(dialogState) },
+                            modifier = Modifier.align(Alignment.TopEnd)
+                        ) {
+                            Text(text = "CLOSE", color = colorResource(id = R.color.DarkOrange))
+                        }
+                    }
+                }
+
+            )
+        }
+    }
+
 
     private fun checkNetwork() {
         lifecycleScope.launchWhenStarted {
@@ -323,3 +409,5 @@ class PostDetailsActivity : ComponentActivity() {
         }
     }
 }
+
+
